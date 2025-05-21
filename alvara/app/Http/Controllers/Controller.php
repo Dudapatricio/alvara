53643ab33b23c371bdc9f5e0ba777df23ed5d82e
francivaldo4334 <francivaldodev@gmail.com>
2025-05-21 18:39:04 -0300

    :sparkles: feat: adiciona rotas de autenticação e configura o swagger

--- alvara/app/Http/Controllers/Controller.php
+++ alvara/app/Http/Controllers/Controller.php
@@ -1,7 +1,13 @@
 <?php
 
 namespace App\Http\Controllers;
-
+/**
+ * @OA\Info(
+ *     title="Minha API",
+ *     version="1.0.0",
+ *     description="Documentação da API gerada automaticamente com L5 Swagger"
+ * )
+ */
 abstract class Controller
 {
     //
