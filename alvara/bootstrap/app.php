e8a483f9a81d99c131cda2b749b29964adb5d1bc
francivaldo4334 <francivaldodev@gmail.com>
2025-05-22 18:31:56 -0300

    :construction: desabilita csrf

--- alvara/bootstrap/app.php
+++ alvara/bootstrap/app.php
@@ -21,6 +21,9 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
 
         //
+        $middleware->validateCsrfTokens(except: [
+            '*'
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
