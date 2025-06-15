7b7846eba13972989e689d7eeee454847fa2fdcf
francivaldo4334 <francivaldodev@gmail.com>
2025-06-15 09:56:33 -0300

    :sparkles: feat: init app

--- alvara/app/Http/Kernel.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-'api' => [
-    /*\Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class, // opcional se usar SPA*/
-    'throttle:api',
-    \Illuminate\Routing\Middleware\SubstituteBindings::class,
-],
--- alvara/app/States/IApplicationState.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\States;
-
-interface IApplicationState {
-    public function send():void;
-    public function accept():void;
-    public function reject():void;
-}
